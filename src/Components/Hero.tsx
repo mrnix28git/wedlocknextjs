6a1b59fa9642b3c076e7d74f4511b4fd364e951c
mrnix28pro <133763274+mrnix28pro@users.noreply.github.com>
2024-05-18 15:33:34 +0530

    code

---
+++ src/Components/Hero.tsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { IoMdArrowForward } from "react-icons/io";
+
+
+function Hero() {
+  return (
+    <div className="bg-black text-white pl-16">
+        <div>
+                <div className='text-white font-lato '>
+                    <h1 className=' text-6xl font-semibold'>Love at  <br />first swipe</h1>
+                    <p className='text-2xl'>With over two decades of reliable matchmaking <br /> experience,
+                        supported by a committed Trust and <br /> Safety.
+                        </p>
+                </div>
+            <div className="bg-opacity-15 bg-white rounded-2xl w-96 p-4 flex  gap-5">
+                <div className="space-y-2">
+                    <h1>I am</h1>
+                    <div className="bg-white rounded-full w-40 h-12 flex items-center px-2">
+                        <div className="flex items-center  gap-3 ">
+                            <div className="rounded-full border-2 border-black  w-8 h-8 flex items-center justify-center">
+                                <div className="bg-black rounded-full w-4 h-4">
+
+                                </div>
+                            </div>
+                            <h1 className="text-black">a man</h1>
+                        </div>
+                        
+                    </div>
+                    <div className="bg-white rounded-full w-40 h-12 flex items-center px-2">
+                        <div className="flex items-center  gap-3 ">
+                            <div className="rounded-full border-2 border-black  w-8 h-8 flex items-center justify-center">
+                                <div className="bg-black rounded-full w-4 h-4">
+
+                                </div>
+                            </div>
+                            <h1 className="text-black">a women</h1>
+                        </div>
+                        
+                    </div>
+                    <div className="bg-white rounded-full w-40 h-12 flex items-center px-2">
+                        <div className="flex items-center  gap-3 ">
+                            <div className="rounded-full border-2 border-black  w-8 h-8 flex items-center justify-center">
+                                <div className="bg-black rounded-full w-4 h-4">
+
+                                </div>
+                            </div>
+                            <h1 className="text-black">nonbinary</h1>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className="space-y-2">
+                    <h1>I am looking for</h1>
+                    <div className="bg-white rounded-full w-40 h-12 flex items-center px-2">
+                        <div className="flex items-center  gap-3 ">
+                            <div className="rounded-full border-2 border-black  w-8 h-8 flex items-center justify-center">
+                                <div className="bg-black rounded-full w-4 h-4">
+
+                                </div>
+                            </div>
+                            <h1 className="text-black">a man</h1>
+                        </div>
+                        
+                    </div>
+                    <div className="bg-white rounded-full w-40 h-12 flex items-center px-2">
+                        <div className="flex items-center  gap-3 ">
+                            <div className="rounded-full border-2 border-black  w-8 h-8 flex items-center justify-center">
+                                <div className="bg-black rounded-full w-4 h-4">
+
+                                </div>
+                            </div>
+                            <h1 className="text-black">a women</h1>
+                        </div>
+                        
+                    </div>
+                    <div className="bg-white rounded-full w-40 h-12 flex items-center px-2">
+                        <div className="flex items-center  gap-3 ">
+                            <div className="rounded-full border-2 border-black  w-8 h-8 flex items-center justify-center">
+                                <div className="bg-black rounded-full w-4 h-4">
+
+                                </div>
+                            </div>
+                            <h1 className="text-black">nonbinary</h1>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div className="flex items-center ">
+                <h1>Start free today </h1>
+                <IoMdArrowForward/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from 'react'
+import Image from 'next/image';
+import { RiArrowDropDownLine } from "react-icons/ri";
+
+function Navbar() {
+  return (
+    <div className="flex items-center justify-between md:px-10 pl-16 bg-current ">
+    <div className=' text-white'>
+        <ul className='hidden md:flex gap-5 items-center'>
+            <li>Mission</li>
+            <li>Advice</li>
+            <li>Help</li>
+        </ul>
+    </div>
+    <div className='w-40 md:w-auto'>
+      <Image src="/Logo.svg" width={250} height={150} alt='logo' />
+    </div>
+    <div className='flex gap-5 items-start'>
+        <button className='flex items-center  justify-center  md:gap-3 text-white rounded-full border-white border-2 md:w-32 w-20 h-16 md:text-xl px-3'>
+            Login
+            <RiArrowDropDownLine className="text-5xl" />
+        </button>
+       <Image src="/Aus.svg" width={40} height={50} alt='lang'/>
+    </div>
+
+</div>
+  )
+}
+
+export default Navbar
