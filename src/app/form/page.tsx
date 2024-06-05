ee5d27688c93000bd5ab958114de22f25628a3a5
mrnix28pro <133763274+mrnix28pro@users.noreply.github.com>
2024-06-05 15:51:33 +0530

    Form

---
+++ src/app/form/page.tsx
@@ -0,0 +1,13 @@
+import Page1 from '@/Components/Forms/Personal'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <Page1/>
+        <Qualifi
+    </div>
+  )
+}
+
+export default page
